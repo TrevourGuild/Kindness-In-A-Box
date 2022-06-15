811453671c18e243967e6299733466d1f11ee268
TrevourGuild <trevorr.guildd@gmail.com>
2022-06-15 13:14:27 -0700

    got checkout to link to success page

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,10 +1,17 @@
-import { Add, Remove, Satellite } from '@material-ui/icons'
+import { Add, Remove,  } from '@material-ui/icons'
 import { useSelector } from 'react-redux'
 import styled from 'styled-components'
 import Announcement from '../components/Announcement'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import { mobile } from '../responsive'
+import StripeCheckout from 'react-stripe-checkout'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import { userRequest} from "../requestMethods"
+import { useHistory } from 'react-router-dom'
+
+const KEY = process.env.REACT_APP_STRIPE
 
 const Container = styled.div`
 
@@ -139,7 +146,28 @@ const Button = styled.button`
 
 
 const Cart = () => {
-    const cart = useSelector(state => state.cart)
+    const cart = useSelector((state) => state.cart)
+    const [stripeToken, setStripeToken] = useState(null)
+    const history = useHistory()
+
+    const onToken = (token)=>{
+        setStripeToken(token)
+    }
+
+    useEffect(()=>{
+        const makeRequest = async ()=>{
+            try{
+            const res = await userRequest.post("/checkout/payment",{
+                tokenId: stripeToken.id,
+                amount: 500,
+            })
+            history.push("/success", { 
+                stripeData:res.data,
+                products: cart, })
+            } catch {}
+        }
+       stripeToken && makeRequest()
+    }, [stripeToken, cart.total, history, cart])
   return (
     <Container>
         <Navbar/>
@@ -197,7 +225,18 @@ const Cart = () => {
                         <SummaryItemText>Total</SummaryItemText>
                         <SummaryItemPrice>$ {cart.total}</SummaryItemPrice>
                     </SummaryItem>
-                    <Button>CHECKOUT NOW</Button>
+                    <StripeCheckout
+                        name="KindBoxCo"
+                        image="https://i.postimg.cc/zBLxBHHW/IMG-1914.jpg"
+                        billingAddress
+                        shippingAddress
+                        description={`Your total is $${cart.total}`}
+                        amount={cart.total*100}
+                        token={onToken}
+                        stripeKey={KEY}
+                    >
+                        <Button>CHECKOUT NOW</Button>
+                    </StripeCheckout>
                 </Summary>
             </Bottom>
         </Wrapper>
