01066748b9eaf7e1d32298e2823c7beac4594ec1
TrevourGuild <trevorr.guildd@gmail.com>
2022-06-24 13:39:41 -0700

    created add update delete to admin page for orders

--- admin/src/redux/apiCalls.js
+++ admin/src/redux/apiCalls.js
@@ -1,4 +1,20 @@
-import { publicRequest } from "../requestMethods";
+import { publicRequest, userRequest } from "../requestMethods";
+
+import { 
+    addProductFailure,
+    addProductStart,
+    addProductSuccess,
+    deleteProductFailure, 
+    deleteProductStart, 
+    deleteProductSuccess, 
+    getProductFailure, 
+    getProductStart, 
+    getProductSuccess,
+    updateProductFailure,
+    updateProductStart,
+    updateProductSuccess,
+} from "./productRedux";
+
 import { loginFailure, loginStart, loginSucess } from "./userRedux";
 
 export const login = async (dispatch, user) =>{
@@ -6,7 +22,45 @@ export const login = async (dispatch, user) =>{
     try{
         const res = await publicRequest.post("/auth/login", user)
         dispatch(loginSucess(res.data))
-    }catch{
+    }catch (err) {
         dispatch(loginFailure())
+    }
+}
+
+export const getProducts = async (dispatch) =>{
+    dispatch(getProductStart());
+    try{
+        const res = await publicRequest.get("/products")
+        dispatch(getProductSuccess(res.data))
+    }catch (err) {
+        dispatch(getProductFailure())
+    }
+}
+
+export const deleteProduct = async (id, dispatch) =>{
+    dispatch(deleteProductStart());
+    try{
+        // const res = await userRequest.delete(`/products${id}`)
+        dispatch(deleteProductSuccess(id))
+    }catch (err) {
+        dispatch(deleteProductFailure())
+    }
+}
+export const updateProduct = async (id, product, dispatch) =>{
+    dispatch(updateProductStart());
+    try{
+        // update
+        dispatch(updateProductSuccess({id, product}))
+    }catch (err) {
+        dispatch(updateProductFailure())
+    }
+}
+export const addProduct = async (product, dispatch) =>{
+    dispatch(addProductStart());
+    try{
+        const res = await userRequest.post(`/products`, {product})
+        dispatch(addProductSuccess(res.data))
+    }catch (err) {
+        dispatch(addProductFailure())
     }
 }
